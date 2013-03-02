d3aaba4f6b506b2184bc4fb571cfd177587c264b
Luc Bourlier <luc.bourlier@typesafe.com>
2013-03-02 16:27:51 +0100

    Adds parsing of the incoming request in server
    Extra the details of the request, with exceptions support

--- src/main/scala/org/skyluc/deluge/ServerActor.scala
+++ src/main/scala/org/skyluc/deluge/ServerActor.scala
@@ -6,19 +6,29 @@ import akka.event.LoggingReceive
 import akka.event.Logging
 import spray.http.HttpRequest
 import spray.io.IOBridge.Closed
+import spray.http.IllegalRequestException
+import spray.http.HttpResponse
+import spray.http.StatusCodes
 
 class ServerActor extends Actor {
 
-  val log = Logging(context.system, this)
-  
-  val router = context.system.actorOf(Props[RouterActor], name = "router")
+  private val log = Logging(context.system, this)
+
+  private val router = context.system.actorOf(Props[RouterActor], name = "router")
 
   def receive = {
     case request: HttpRequest =>
-      router forward request
+      try {
+        val parsedRequest = request.parseAll
+        router forward parsedRequest
+      } catch {
+        case e: IllegalRequestException =>
+          log.error(e, "Exception while parsing request")
+          sender ! HttpResponse(StatusCodes.InternalServerError, entity = "Something went wrong. Sorry.")
+      }
     case _: Closed =>
-      // nothing to do
+    // nothing to do
     case m =>
       log.info("received unknown message")
-  }  
+  }
 }
