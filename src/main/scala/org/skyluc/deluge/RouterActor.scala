f3962700bcd8bc19cb104cc9ef130197caeb474a
Luc Bourlier <luc.bourlier@typesafe.com>
2013-03-02 16:28:26 +0100

    Implements the router
    Use a filter to forward the request to the right handler.
    With support to list all resquets made, and the invalid ones.

--- src/main/scala/org/skyluc/deluge/RouterActor.scala
+++ src/main/scala/org/skyluc/deluge/RouterActor.scala
@@ -3,12 +3,60 @@ package org.skyluc.deluge
 import akka.actor.Actor
 import akka.event.LoggingReceive
 import spray.http.HttpResponse
+import akka.actor.ActorRef
+import akka.actor.Props
+import spray.http.HttpRequest
+import spray.http.HttpResponse
+import spray.http.StatusCodes
+import akka.event.Logging
+
+// messages
+case class HandlerDefinition(id: String, actorClass: Class[_ <: Actor], filter: String => Boolean)
+
+// model
+case class Handler(actor: ActorRef, filter: String => Boolean)
 
 class RouterActor extends Actor {
 
+  val log = Logging(context.system, this)
+  
+  private val allRequestsHandler = context.system.actorOf(Props[ListRequestsActor], name = "allRequests")
+  private val invalidRequestsHandler = context.system.actorOf(Props[ListRequestsActor], name = "invalidRequests")
+  
+  var handlers : List[Handler]= List(
+      Handler(allRequestsHandler, new StringMatchFilter("/deluge/router/allRequests")),
+      Handler(invalidRequestsHandler, new StringMatchFilter("/deluge/router/invalidRequests")));
+  
   def receive = LoggingReceive {
-    case _ =>
-      sender ! HttpResponse(entity = "ok")
+    case HandlerDefinition(id, actorClass, filter) =>
+      handlers = handlers :+ Handler(context.actorOf(Props(actorClass), name = id), filter)
+    case request: HttpRequest =>
+      allRequestsHandler ! AddRequestMessage(request)
+      handlers.find(_.filter(request.path)) match {
+        case Some(handler) =>
+          handler.actor forward request
+        case None =>
+          invalidRequestsHandler ! AddRequestMessage(request)
+          sender ! HttpResponse(StatusCodes.NotFound, "%s not found".format(request.uri))
+      }
   }
   
+}
+
+class StringMatchFilter(path: String) extends Function1[String, Boolean] {
+  def apply(toCheck: String): Boolean = path == toCheck
+}
+
+case class AddRequestMessage(request: HttpRequest)
+
+class ListRequestsActor extends Actor {
+  
+  private var requests: Set[String] = Set()
+  
+  def receive = {
+    case _: HttpRequest =>
+      sender ! HttpResponse(entity = requests.mkString("\n"))
+    case AddRequestMessage(request) =>
+      requests += request.path
+  }
 }
